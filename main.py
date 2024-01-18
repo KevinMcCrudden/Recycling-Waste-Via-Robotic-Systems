d4c20870c6a9759dae0bd9e544cebae21538d6de
KevinMcCrudden <lilkevinmccrudden@gmail.com>
2024-01-18 09:14:38 -0500

    Added column function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import column, row
 import pandas
 
 # Read in CSV
@@ -7,10 +8,7 @@ df = pandas.read_csv('cars.csv')
 car = df['Car']
 hp = df['Horsepower']
 
-
-output_file("index.html")
-
-# Add plot
+# Add plot for orange cars
 p = figure(
     y_range=car,
     width=800,
@@ -20,7 +18,7 @@ p = figure(
     tools="pan,box_select,zoom_in,zoom_out,save,reset"
 )
 
-# Render glyph
+# Render glyph for orange cars
 p.hbar(
     y=car,
     right=hp,
@@ -30,5 +28,25 @@ p.hbar(
     fill_alpha=0.5
 )
 
+# Add plot for green cars
+g = figure(
+    y_range=car,
+    width=800,
+    height=600,
+    title="Cars with Top Horsepower",
+    x_axis_label="Horsepower",
+    tools="pan,box_select,zoom_in,zoom_out,save,reset"
+)
+
+# Render glyph for green cars
+g.hbar(
+    y=car,
+    right=hp,
+    left=0,
+    height=0.4,
+    color="green",
+    fill_alpha=0.5
+)
+
 # Show results
-show(p)
+show(column(p, g))

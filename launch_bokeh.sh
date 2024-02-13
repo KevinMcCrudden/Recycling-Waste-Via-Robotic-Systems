fe3b0324fca07431a10e5e43f9da6f9b200e107b
KevinMcCrudden <lilkevinmccrudden@gmail.com>
2024-02-13 15:44:37 -0500

    Added local netowork support

--- launch_bokeh.sh
+++ launch_bokeh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start Bokeh server and save its PID
-bokeh serve main.py --10.0.4.103 --port 5006 &
+bokeh serve main.py --address 10.0.4.103 --port 5006 --allow-websocket-origin=10.0.4.103:5006 &
 echo $! > bokeh-server.pid

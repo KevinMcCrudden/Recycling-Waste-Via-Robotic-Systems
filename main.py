cf6220669b7f08b832c5e3bd3fe441d5b2a8237c
KevinMcCrudden <lilkevinmccrudden@gmail.com>
2024-02-13 17:13:10 -0500

    Still having issues

--- main.py
+++ main.py
@@ -427,12 +427,12 @@ Robot_Rate_Layout.sizing_mode = "scale_both"
 def create_models():
     # Your code to create new models goes here
     # For example, creating new Tabs
-    Locations_Panel = TabPanel(child=plots_Locations(), title="WPI Recycling Locations") 
+    Locations_Panel = TabPanel(child=plots_Locations, title="WPI Recycling Locations") 
     Monthly_panel = TabPanel(child=plots_monthly, title="22 WPI Recycling Monthly")
     Academic_Year_panel = TabPanel(child=academic_year, title="WPI Recycling Academic Year")
     Robot_panel = TabPanel(child=Robot_Rate_Layout, title="Robot Calculation")
 
-    tabs = Tabs(tabs=[Locations_Panel, Monthly_panel(), Academic_Year_panel(), Robot_panel()])
+    tabs = Tabs(tabs=[Locations_Panel, Monthly_panel, Academic_Year_panel, Robot_panel])
     return tabs
 
 # Updated modify_doc function
